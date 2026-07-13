251470b87d2f79a884670abe1cda79d12e7089ed
Sara <s.cristal.p@gmail.com>
2026-07-13 22:10:12 +0200

    bookcard

--- src/pages/BookCard.jsx
+++ src/pages/BookCard.jsx
@@ -1,7 +1,20 @@
-import BooksPage from "./BooksPage";
+import { NavLink } from "react-router-dom";
 
 export default function BookCard({ book }) {
-    return (
-        <div></div>
-    );
+  return (
+    <article>
+      <img src={book.coverimage} alt={book.title} />
+
+      <h2>{book.title}</h2>
+
+      <p>{book.author}</p>
+
+      <p>{book.available ? "Available" : "Checked Out"}</p>
+
+      <NavLink to={`/books/${book.id}`}>
+        View Details
+      </NavLink>
+    </article>
+  );
 }
+//pass details from BooksPage
