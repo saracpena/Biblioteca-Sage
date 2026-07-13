4ef5bba3a2ec8e58f47089cf7a122ac7cb39993a
Sara <s.cristal.p@gmail.com>
2026-07-13 19:41:26 +0200

    main.jsx comments why BR

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,15 @@ createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>,
 );
+
+/* Once BrowserRouter is in place, React Router gives us access to things like:
+
+<Routes>
+<Route>
+<Link>
+<NavLink>
+<Outlet>
+useNavigate()
+useParams()
+
+Without BrowserRouter, none of those work. */
