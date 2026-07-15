c79e652bd7883ca19d2ec8398ab8283b195190d5
Sara <s.cristal.p@gmail.com>
2026-07-15 21:01:38 +0200

    add provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
 import "./App.css";
-import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+  <AuthProvider>
     <App />
+  </AuthProvider>
   </BrowserRouter>,
 );
 
