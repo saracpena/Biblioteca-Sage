488e3a57c7c39b9174e655072b3953cddb3a7eee
Sara <s.cristal.p@gmail.com>
2026-07-15 20:52:41 +0200

    import Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import { Routes, Route } from "react-router";
+import Navbar from "./components/Navbar";
 import BooksPage from "./pages/BooksPage";
 import BooksDetailsPage from "./pages/BooksDetailsPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-<>
-  <header className="app-header">
-    <img
-      id="logo-image"
-      src="/books.png"
-      alt="Biblioteca Sage logo"
-    />
+    <>
+      <header className="app-header">
+        <img id="logo-image" src="/books.png" alt="Biblioteca Sage logo" />
 
-    <h1 className="app-title">Biblioteca Sage</h1>
-  </header>
-
-  <main>
-    <Routes>
-      <Route path="/" element={<BooksPage />} />
-      <Route path="/books" element={<BooksPage />} />
-      <Route path="/books/:id" element={<BooksDetailsPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
-  </main>
-</>
+        <h1 className="app-title">Biblioteca Sage</h1>
+      </header>
+      <Navbar/>
+      <main>
+        <Routes>        
+          <Route path="/" element={<BooksPage />} />
+          <Route path="/books" element={<BooksPage />} />
+          <Route path="/books/:id" element={<BooksDetailsPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
